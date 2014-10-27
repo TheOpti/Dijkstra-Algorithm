e0217d48e2b8799cc7d54d09c4880fe3ce71498a
Piotr Więckowski <thespyker@wp.pl>
2014-10-27 20:13:57 +0100

    Initial commit

---
+++ src/main/java/opti/dijkstraalgorithm/Graph/Edge.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opti.dijkstraalgorithm.Graph;
+
+/**
+ *
+ * @author opti
+ */
+public class Edge {
+    
+    private Node from;
+    private Node to;
+    private Integer cost;
+    
+    public Edge() {
+        
+    }
+    
+    public Edge(Node from, Node to) {
+        this.from = from;
+        this.to = to;
+    }
+    
+    public Node getFrom() {
+        return this.from;
+    }
+    
+    public Node getTo() {
+        return this.to;
+    }
+    
+    public Integer getCost() {
+        return this.cost;
+    }
+    
+    public void setFrom(Node from) {
+        this.from = from;
+    }
+    
+    public void setTo(Node to) {
+        this.to = to;
+    }
+    
+    
+}
---
+++ src/main/java/opti/dijkstraalgorithm/Graph/Node.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opti.dijkstraalgorithm.Graph;
+
+import java.util.List;
+
+/**
+ *
+ * @author opti
+ */
+public class Node {
+    
+    private String name;
+    private List<Edge> edges;
+    
+    
+    
+}
