f02d61e16c5dcac1f669efb2175f3ed139fe5224
Piotr Więckowski <thespyker@wp.pl>
2014-10-27 20:24:15 +0100

    Dependencies, added imports

--- src/main/java/Graph/Edge.java
+++ src/main/java/Graph/Edge.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package opti.dijkstraalgorithm.Graph;
+package Graph;
 
 /**
  *
--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
+import Graph.Graph;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -19,10 +12,11 @@ public class GraphTest {
     public GraphTest() {
     }
     
+    @Test
+    public void testGraph() {
+        Graph graph = new Graph();
+        Assert.assertNotNull(graph);
+    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+
 }
